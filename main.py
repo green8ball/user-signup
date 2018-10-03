25345c316eab0b7f7841599b65ec2a63219d6736
Joshua Ortmann <joshuaortmann@gmail.com>
2018-10-03 19:23:54 -0400

    Removed unneccessary is_blank function

--- main.py
+++ main.py
@@ -21,25 +21,15 @@ def create_user():
     f_blank = "Error: Field blank"
     password_mismatch = "Error: Password Mismatch"
 
-    field_blank = is_blank(username, password, verify)
-    
-    #password_valid
-
     template = jinja_env.get_template('signup.html')
     return template.render(username=username, username_error=f_blank if username=="" else "", 
                             password="", password_error=f_blank if password=="" else password_mismatch if password!=verify else "",
-                            verify="", verify_error=f_blank if verify=="" else "" else password_mismatch if password!=verify else "",
+                            verify="", verify_error=f_blank if verify=="" else password_mismatch if password!=verify else "",
                             email=email)
-
-def is_blank(usr, pw, v_pw):
-    if usr == "" or pw == "" or v_pw == "":
-        return True
-    else:
-        return False
 
 @app.route("/")
 def index():
     template = jinja_env.get_template('signup.html')
-    return template.render(username="",email="")
+    return template.render()
 
 app.run()
