da6c9dd1facf5d9d082af9661b5316f4e830744b
Joshua Ortmann <joshuaortmann@gmail.com>
2018-10-03 20:25:49 -0400

    Implemented welcome.html within return statement of create_user function.

--- main.py
+++ main.py
@@ -24,18 +24,19 @@ def create_user():
     error_email_invalid = "Error: Invalid Email"
     error_username_invalid = "Error: Invalid Username"
     
-    is_email_valid = re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)",email)
-    is_username_valid = re.match("^([a-zA-Z0-9@*#]{3,20})$",username)
-    is_password_valid = re.match("^([a-zA-Z0-9@*#]{3,20})$",password)
+    is_email_valid = re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email)
+    is_username_valid = re.match("^([a-zA-Z0-9@*#]{3,20})$", username)
+    is_password_valid = re.match("^([a-zA-Z0-9@*#]{3,20})$", password)
 
     template_index = jinja_env.get_template('signup.html')
+    template_welcome = jinja_env.get_template('welcome.html')
     if (is_email_valid == None and email != "") or is_username_valid == None or username == "" or is_password_valid == None or password != verify or password == "":
         return template_index.render(username = username, username_error = error_field_blank if username == "" else error_username_invalid if is_username_valid == None else "" , 
                                 password = "", password_error = error_field_blank if password == "" else error_password_mismatch if password != verify else error_password_invalid if is_password_valid == None else "",
                                 verify = "", verify_error = error_field_blank if verify == "" else error_password_mismatch if password != verify else error_password_invalid if is_password_valid == None else "",
                                 email = email, email_error = "" if email == "" else error_email_invalid if is_email_valid == None else "")
     else:
-        return "<h1>Welcome, {0}!".format(username)
+        return template_welcome.render(username = username)
 
 @app.route("/")
 def index():
